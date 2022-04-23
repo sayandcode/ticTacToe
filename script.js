c17cf2b3d5eec3505c829b445a00e7b3e670977f
sayandcode <sayandcode@gmail.com>
2022-04-23 11:37:09 +0530

    Add new logic
    This uses the cell value to store the changes in row/column/diag sum.
    Each cell know which row/column/diag it belongs to.

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
+// format= 0 000 0 001 0 001 0 100 0 001 0 001 0 000 0 001 
+// format= 0 aDi 0 Dia 0 Co3 0 Co2 0 Co1 0 Rw3 0 Rw2 0 Rw1 
+const board = (function(){
 function findWinCombos(n){
-  let winCombos=[];
-  let cell=[];
-  
-  let rowSum=0;
-  let colSum=Array(n).fill(0);
-  let diagSum=[0,0];
-  
-  for (let i = 0; i < n**2; i++) {
-    cell[i]=2**i; //assign cell values
+  let cell=[]; 
+  let indenter=0;
+  for (let i = 0; i < n*n; i++) {  //for each cell
+    cell[i]=0;
 
-    //add the respective values to the sum counters
-    rowSum+=cell[i];
-    colSum[i%n]+=cell[i];
-    if((i)%(n+1)===0)
-      diagSum[0]+=cell[i];
-    if(((i%(n-1))===0)&&(i !==(n**2-1))&&(i!==0)) //exclude the first&last cell, which happens to satisfy the first condition
-      diagSum[1]+=cell[i];
+    //add to appropriate row.
+    if((i%n===0)&&(i>0))  //pad with 0 every n+1th cell
+      indenter++;
+    cell[i]+=2**(i+indenter);
 
-    //check if end of row & push to winCombos
-    if((i+1)%n===0){ 
-      winCombos.push(rowSum);
-      rowSum=0; //need to reset this, cause we're reusing one variable for every row
-    }
+    //add to appropriate column
+    cell[i]+=2**(i+(n*(n+1))+indenter)
+
+    //add to appropriate diagonal 
+    if(i%(n+1)===0)                                     //if its on the idagonal    
+      cell[i]+=2**( (2*n*(n+1)) + (Math.round(i/4)) );  //diagonal value is stored after the [n rows+ n columns]*(n+1) digits for each
+      
+    //add to appropriate antiDiagonal 
+    if ( (i%(n-1) === 0) && (i!==0) && (i!==(n**2 - 1)) ) //excludes first and last element, which also happen to satisfy this condition
+      cell[i]+= 2**( (2*n + 1)*(n+1) + i/(n-1) - 1);      //antiDiagonal value is stored after [n rows + n columns + 1diag]*(n-1) digits for each
   }
-  
-  //since we're persisitently storing the other two variables, we can just push them at the end of the loop
-  winCombos.push(...colSum,...diagSum); 
 
-  return winCombos.map(combo=>combo.toString(2));
+  return cell;
 }
-  
+
+/* BELOW IS A FUNCTION FOR YOU TO SEE THE FORM OF EACH CELL VALUE, AS CONCEPTUALIZED WITH SPACES */
+/* IT IS NOT USED IN THE FUNCTION, AND IS SOLELY FOR YOUR UNDERSTANDING */
+// function displayNbits(matrix){          
+//   let n=matrix.length**0.5
+//   let N=(2*n+2)*(n+1);
+//   result=matrix.map(cell=>{
+//     num=cell.toString(2);
+//     while(num.length<N){                                   
+//       num='0'+num;
+//     }
+//     const regex=new RegExp(`(.)(.{${n}})`,'g')
+//     num=[...num.replace(regex,'$1 $2 ')];
+//     num=num.join('');
+//     return num;
+//   });  
+//   console.table(result);
+// }
+
+return {
+  findWinCombos
+}
+
+})();
